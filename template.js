a581ad06a91cc6e9119a60c29feb7029a90890be
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-07-26 16:25:33 +0900

    fix grunt-simple-ejs change.

--- template.js
+++ template.js
@@ -39,7 +39,7 @@ exports.template = function (grunt, init, done) {
                 'grunt-contrib-compass': '0.3.0',
                 'grunt-auto-deps': '0.0.3',
                 'grunt-koko': '~0.1.0',
-                "grunt-simple-ejs": "0.0.0",
+                "grunt-simple-ejs": "0.1.0",
                 'grunt-mocha-html': '0.0.1',
                 'grunt-mocha-phantomjs': '~0.2.8',
                 'mocha': '~1.9.0',
