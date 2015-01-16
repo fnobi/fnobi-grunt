ce0076c7816bdaa3f347bed15e5f7d8ca4a544a3
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-01-17 08:19:24 +0900

    importerちょっと拡張

--- root/src/import.sh
+++ root/src/import.sh
@@ -6,10 +6,19 @@ api="http://api.tumblr.com/v2/blog/$blog/posts"
 # # filter text posts
 # api=$api"/text"
 
+# set api key
+api=$api"?api_key=$api_key"
+
+# # with note info
+# api=$api"&notes_info=true"
+
+# # filter pickup tagged
+# api=$api"&tag=pickup"
+
 if [ ! -e $data_dir ] ; then
     mkdir -p $data_dir
 fi
 
-curl "$api?api_key=$api_key&notes_info=true" > $data_dir/api.json
+curl "$api" > $data_dir/api.json
 cat $data_dir/api.json | jq .response.blog > $data_dir/blog.json
 cat $data_dir/api.json | jq .response.posts > $data_dir/posts.json
