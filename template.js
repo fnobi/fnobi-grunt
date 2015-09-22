5610bd3e1e4a8c30ee9635bcaac57df3beda070c
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-09-22 18:46:53 +0900

    update template description.

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 var escapeFiles = require('./lib/escapeFiles'),
     shellLines = require('./lib/shellLines');
 
-exports.description = 'web page template';
+exports.description = 'Web site template with gulp, jade, compass and varline.';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = '';
