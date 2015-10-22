5942a243bfa3fb9cc5aa780226f7c6d1beb9975e
Fujisawa Shin <fujisawa-shin@kayac.com>
2015-10-22 12:18:43 +0900

    version整理・reset css変えた

--- template.js
+++ template.js
@@ -142,9 +142,9 @@ exports.template = function (grunt, init, done) {
             version: props.version,
             main: 'index.html',
             dependencies: {
-                'html5shiv': '~3.7.2',
-                'jquery': '~1.11.*',
-                'normalize-css': 'normalize.css#~3.0.*'
+                'html5shiv': '3.7.*',
+                'jquery': '1.11.*',
+                'reset-css': '2.0.*'
             }
         };
 
